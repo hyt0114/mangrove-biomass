de0e39251c5bbaa15877986166cffb2f466cf231
胡溢添 <huyitian@hadecj.onaliyun.com>
2024-03-29 16:54:22 +0800

    feat: 添加微信小程序版本更新提示器

--- App.vue
+++ App.vue
@@ -4,7 +4,32 @@
 			console.log('App Launch')
 		},
 		onShow: function() {
-			console.log('App Show')
+			//#ifdef MP-WEIXIN
+			console.log("小程序")
+			const updateManager = wx.getUpdateManager()
+
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				console.log(res.hasUpdate)
+			})
+
+			updateManager.onUpdateReady(function() {
+				wx.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function(res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate()
+						}
+					}
+				})
+			})
+
+			updateManager.onUpdateFailed(function() {
+				// 新版本下载失败
+			})
+			//#endif
 		},
 		onHide: function() {
 			console.log('App Hide')
